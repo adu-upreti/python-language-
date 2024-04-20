8f3f09a4dcd94eadb208d887313657b44a69b3f0
adu-upreti <162395567+adu-upreti@users.noreply.github.com>
2024-04-20 18:26:14 +0545

    Add files via upload

---
+++ game.py
@@ -0,0 +1,18 @@
+
+import random
+random_number = random.randint(1, 100)
+print("random number:", random_number)
+
+guesses = 0
+
+while True:
+    num = int(input("Enter the guessing Value:"))
+    guesses += 1
+
+    if num == random_number:
+        print("Congrats!", num, "is correct number. you have guess the answer in just", guesses, "time.")
+        break
+    elif num < random_number:
+        print("hints. just above than your input")
+    else:
+        print("hints. just less than your input")
