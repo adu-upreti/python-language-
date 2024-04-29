ca43b2a3f48a6d16ce70c63d1fc68f1b681af565
adu-upreti <162395567+adu-upreti@users.noreply.github.com>
2024-04-28 20:11:15 -0700

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,16 @@
+print("#####################CALCULATOR###################")
+a = int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+c = str(input("choose operator:"))
+
+if c == "+":
+    print("Ans =",a+b)
+
+elif c == "-":
+    print("Ans =",a-b)
+
+elif c == "*":
+    print("Ans =",a*b)
+
+elif c == "/":
+    print("Ans =",a/b)
